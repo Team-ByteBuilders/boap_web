560af5b9595c45d079a7d9a0896c45b7401904b3
shreyal0007 <shreyaljain0007@gmail.com>
2023-03-24 18:00:44 +0530

    payment reminder added

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,30 +1,46 @@
 const User = require("../models/userModel");
+const Payment=require("../models/paymentModel");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-//Register and login
+//Payment
 const sendPayment = async (req, res) => {
   try {
     const { upiId, amount } = req.body;
 
-    if (!upiId) {
+    if (!upiId || !amount) {
       {
         return res.status(400).json({ message: "incomplete data recieved :)" });
       }
     }
     const user = await User.findById(req.userID);
-    const user2 = await User.findOne((userUpi = upiId));
+    const user2 = await User.findOne({userUpi : upiId});
     if (user.balance - amount < 0) {
       return res.status(400).json({ message: "insufficient balance :)" });
     }
-    const sUser = await User.findByIdAndUpdate(req.userID, {
+    let sUser = await User.findByIdAndUpdate(req.userID, {
       balance: user.balance - amount,
     });
-    await User.findOneAndUpdate((userUpi = upiId), {
+    sUser = await User.findByIdAndUpdate(req.userID, {
+      paymentHistory: [...sUser.paymentHistory, {
+        from: sUser.upiId,
+        to: upiId,
+        amount: amount
+      }]
+    });
+    const rUser=await User.findOneAndUpdate({userUpi : upiId}, {
       balance: user2.balance + amount,
     });
+    const payment = await new Payment({
+      from: sUser.upiId,
+      to:rUser.upiId, 
+      amount:amount,
+    });
+    //save user to database and return response
+    const newPayment = await user.save();
     return res.status(200).json({
       balance: sUser.balance,
+      newPayment
     });
   } catch (error) {
     res.status(500).json({
@@ -33,7 +49,66 @@ const sendPayment = async (req, res) => {
     });
   }
 };
+//split bill notification
+const paymentReminder=async(req,res)=>{
+ try {
+   const { myamount,amount,participants ,upiId} = req.body;
+   if(!myamount || !amount || !participants){
+    return res.status(400).json({message:"enter all the inputs"})
+  }
+  const user = await User.findById(req.userID);
+  const user2 = await User.findOne({ userUpi: upiId });
+  if (user.balance - amount < 0) {
+    return res.status(400).json({ message: "insufficient balance :)" });
+  }
+  let sUser = await User.findByIdAndUpdate(req.userID, {
+    balance: user.balance - amount,
+  });
+  sUser = await User.findByIdAndUpdate(req.userID, {
+    pendingSettlements: [
+      ...sUser.pendingSettlements,
+      {
+        from: sUser.upiId,
+        to: upiId,
+        amount: amount,
+        participants: participants,
+      },
+    ],
 
+    paymentHistory: [
+      ...sUser.paymentHistory,
+      {
+        from: sUser.upiId,
+        to: upiId,
+        amount: amount,
+      },
+    ],
+  });
+  const rUser = await User.findOneAndUpdate(
+    { userUpi: upiId },
+    {
+      balance: user2.balance + amount,
+    }
+  );
+  const payment = await new Payment({
+    from: sUser.upiId,
+    to: rUser.upiId,
+    amount: amount,
+  });
+  //save user to database and return response
+  const newPayment = await user.save();
+  return res.status(200).json({
+    balance: sUser.balance,
+    newPayment,
+  });
+ } catch (error) {
+   res.status(500).json({
+     message: "internal server error occured :)",
+     error: error.message,
+   });
+ }
+}
 module.exports = {
-  login,
+  sendPayment,
+  paymentReminder
 };
